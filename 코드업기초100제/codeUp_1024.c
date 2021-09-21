3c1c2f89c9aa3158d7eefbc392ee7511819d1440
jaehoon9186 <hunhun9186@gmail.com>
2021-09-21 19:06:08 +0900

    ¤·¤·

---
+++ 코드업기초100제/codeUp_1024.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char data[21];
+    scanf("%s", data);
+    for (int i=0; data[i] != '\0'; i++ ){
+        printf("\'%c\'\n", data[i]);
+    }
+    return 0;
+}
